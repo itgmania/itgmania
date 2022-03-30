02008eb469e1c01495c29c0177f563607638e43f
Martin Natano <natano@natano.net>
2022-03-30 22:54:25 +0200

    Rename to ITGmania
    - executable name:
      - linux: itgmania
      - windows: ITGmania.exe
      - macOS: ITGmania.app
    - config path:
      - linux: ~/.itgmania
      - windows; %AppData%\ITGmania
      - macOS: ~/Library/Application Support/ITGmania

--- src/arch/ArchHooks/ArchHooks_Unix.cpp
+++ src/arch/ArchHooks/ArchHooks_Unix.cpp
@@ -412,7 +412,7 @@ void ArchHooks::MountUserFilesystems( const RString &sDirOfExecutable )
 	 * Lowercase the PRODUCT_ID; dotfiles and directories are almost always lowercase.
 	 */
 	const char *szHome = getenv( "HOME" );
-	RString sUserDataPath = ssprintf( "%s/.%s", szHome? szHome:".", "stepmania-5.1" ); //call an ambulance!
+	RString sUserDataPath = ssprintf( "%s/.%s", szHome? szHome:".", "itgmania" );
 	FILEMAN->Mount( "dir", sUserDataPath + "/Announcers", "/Announcers" );
 	FILEMAN->Mount( "dir", sUserDataPath + "/BGAnimations", "/BGAnimations" );
 	FILEMAN->Mount( "dir", sUserDataPath + "/BackgroundEffects", "/BackgroundEffects" );
