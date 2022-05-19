011b17443aab161e0d893d822de45d0a3e8ff10b
Martin Natano <natano@natano.net>
2022-05-19 11:45:45 +0200

    Replace logo and clean up packaging

--- CMake/CPackSetup.cmake
+++ CMake/CPackSetup.cmake
@@ -1,5 +1,5 @@
 set(CPACK_PACKAGE_NAME "${SM_EXE_NAME}")
-set(CPACK_PACKAGE_VENDOR "ITGmania")
+set(CPACK_PACKAGE_VENDOR "${SM_EXE_NAME} Developers")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Rhythm Game Simulator")
 set(CPACK_PACKAGE_VERSION_MAJOR "${SM_VERSION_MAJOR}")
 set(CPACK_PACKAGE_VERSION_MINOR "${SM_VERSION_MINOR}")
@@ -31,28 +31,28 @@ if(WIN32)
   set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON)
   set(CPACK_NSIS_MUI_ICON "${SM_INSTALLER_DIR}/install.ico")
   set(CPACK_NSIS_MUI_UNIICON "${SM_INSTALLER_DIR}/uninstall.ico")
+  set(CPACK_NSIS_MUI_HEADERIMAGE "${SM_INSTALLER_DIR}/header.bmp")
+  set(CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP "${SM_INSTALLER_DIR}/welcome.bmp")
   set(CPACK_NSIS_COMPRESSOR "/SOLID lzma")
-
-  # Custom items for nsis go here.
-  set(CPACK_SM_NSIS_REPOSITORY "https://github.com/itgmania/itgmania")
-  set(CPACK_SM_NSIS_ROOT_DIR "${SM_ROOT_DIR}")
-  set(CPACK_SM_NSIS_PRODUCT_ID "ITGmania ${SM_VERSION_MAJOR}.${SM_VERSION_MINOR}")
-  set(CPACK_SM_NSIS_PRODUCT_VERSION "${SM_VERSION_TRADITIONAL}.0")
-  set(CPACK_SM_NSIS_HEADER_BITMAP "${SM_INSTALLER_DIR}/header-sm5.bmp")
-  set(CPACK_SM_NSIS_WELCOME_BITMAP "${SM_INSTALLER_DIR}/welcome-sm5.bmp")
-  set(CPACK_SM_NSIS_GIT_VERSION "${SM_VERSION_GIT}")
+  set(CPACK_NSIS_MUI_FINISHPAGE_RUN "${SM_EXE_NAME}.exe")
+  set(CPACK_NSIS_BRANDING_TEXT " ")
 
   # The header and welcome bitmaps require backslashes.
   string(REGEX
          REPLACE "/"
                  "\\\\\\\\"
-                 CPACK_SM_NSIS_HEADER_BITMAP
-                 "${CPACK_SM_NSIS_HEADER_BITMAP}")
+                 CPACK_NSIS_MUI_HEADERIMAGE
+                 "${CPACK_NSIS_MUI_HEADERIMAGE}")
   string(REGEX
          REPLACE "/"
                  "\\\\\\\\"
-                 CPACK_SM_NSIS_WELCOME_BITMAP
-                 "${CPACK_SM_NSIS_WELCOME_BITMAP}")
+                 CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP
+                 "${CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP}")
+
+  # Custom items for nsis go here.
+  set(CPACK_SM_NSIS_PRODUCT_ID "ITGmania")
+  set(CPACK_SM_NSIS_PRODUCT_VERSION "${SM_VERSION_TRADITIONAL}.0")
+  set(CPACK_SM_NSIS_GIT_VERSION "${SM_VERSION_GIT}")
 elseif(MACOSX)
   set(CPACK_GENERATOR DragNDrop)
   set(CPACK_DMG_VOLUME_NAME "${CPACK_PACKAGE_NAME} ${CPACK_PACKAGE_VERSION}")
