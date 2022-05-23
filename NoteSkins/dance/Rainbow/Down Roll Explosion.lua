94d41a264fdc555671dcf9fd7044bbc530dbfaf7
teejusb <5017202+teejusb@users.noreply.github.com>
2022-05-22 23:16:36 -0700

    Add Noteskins
    Added:
    - Cel (classic)
    - Metal (classic)
    - Cyber (LightningXCE's)
    - Enchantment
    - Peter's Note
    - Peter's Rainbow
    - Peter's Vivid
    
    Removed:
    - All stock noteskins besides default

---
+++ NoteSkins/dance/Rainbow/Down Roll Explosion.lua
@@ -0,0 +1,14 @@
+local t = Def.ActorFrame {
+	Def.Sprite {
+		Texture=NOTESKIN:GetPath( '_down', 'Hold Explosion' );
+		Frame0000=0;
+		Delay0000=0.01;
+		Frame0001=1;
+		Delay0001=0.01;
+		Frame0002=2;
+		Delay0002=0.01;
+		Frame0003=3;
+		Delay0003=0.01;
+	};
+};
+return t;
---
+++ NoteSkins/dance/Rainbow/Down Tap Fake.lua
@@ -0,0 +1,7 @@
+local t = Def.Model {
+	Meshes=NOTESKIN:GetPath('_down','tap note model');
+	Materials=NOTESKIN:GetPath('_down','tap note model');
+	Bones=NOTESKIN:GetPath('_down','tap note model');
+};
+
+return t;
