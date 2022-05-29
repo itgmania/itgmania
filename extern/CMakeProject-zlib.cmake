7241b477138d79f638f91474bd572c7f12c65ead
Martin Natano <natano@natano.net>
2022-05-29 11:02:58 +0200

    Upgrade zlib 1.2.11 -> 1.2.12 and move to submodule

--- extern/CMakeProject-zlib.cmake
+++ extern/CMakeProject-zlib.cmake
@@ -1,40 +1,37 @@
 if(WITH_SYSTEM_ZLIB)
   find_package(ZLIB REQUIRED)
 else()
-  list(APPEND ZLIB_SRC
-              "zlib/adler32.c"
-              "zlib/compress.c"
-              "zlib/crc32.c"
-              "zlib/deflate.c"
-              "zlib/gzclose.c"
-              "zlib/gzlib.c"
-              "zlib/gzread.c"
-              "zlib/gzwrite.c"
-              "zlib/infback.c"
-              "zlib/inffast.c"
-              "zlib/inflate.c"
-              "zlib/inftrees.c"
-              "zlib/minigzip.c"
-              "zlib/trees.c"
-              "zlib/uncompr.c"
-              "zlib/zutil.c")
+  set(ZLIB_SRC "zlib/adler32.c"
+               "zlib/compress.c"
+               "zlib/crc32.c"
+               "zlib/deflate.c"
+               "zlib/gzclose.c"
+               "zlib/gzlib.c"
+               "zlib/gzread.c"
+               "zlib/gzwrite.c"
+               "zlib/infback.c"
+               "zlib/inffast.c"
+               "zlib/inflate.c"
+               "zlib/inftrees.c"
+               "zlib/trees.c"
+               "zlib/uncompr.c"
+               "zlib/zutil.c")
 
-  list(APPEND ZLIB_HPP
-              "zlib/crc32.h"
-              "zlib/deflate.h"
-              "zlib/gzguts.h"
-              "zlib/inffast.h"
-              "zlib/inffixed.h"
-              "zlib/inflate.h"
-              "zlib/inftrees.h"
-              "zlib/trees.h"
-              "zlib/zconf.h"
-              "zlib/zlib.h"
-              "zlib/zutil.h")
+  set(ZLIB_HPP "zlib/crc32.h"
+               "zlib/deflate.h"
+               "zlib/gzguts.h"
+               "zlib/inffast.h"
+               "zlib/inffixed.h"
+               "zlib/inflate.h"
+               "zlib/inftrees.h"
+               "zlib/trees.h"
+               "zlib/zconf.h"
+               "zlib/zlib.h"
+               "zlib/zutil.h")
 
   source_group("" FILES ${ZLIB_SRC} ${ZLIB_HPP})
 
-  add_library("zlib" ${ZLIB_SRC} ${ZLIB_HPP})
+  add_library("zlib" STATIC ${ZLIB_SRC} ${ZLIB_HPP})
 
   set_property(TARGET "zlib" PROPERTY FOLDER "External Libraries")
 
