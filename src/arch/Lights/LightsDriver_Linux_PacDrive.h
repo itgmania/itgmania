bfba3f608e635dcfa2076eaaac92ce8c8cf67705
teejusb <5017202+teejusb@users.noreply.github.com>
2023-02-17 12:27:58 -0800

    Add Linux Pacdrive

---
+++ src/arch/Lights/LightsDriver_Linux_PacDrive.h
@@ -0,0 +1,55 @@
+#ifndef LIGHTSDRIVER_LINUX_PACDRIVE_H
+#define LIGHTSDRIVER_LINUX_PACDRIVE_H
+
+#include "LightsDriver.h"
+
+extern "C" {
+#include <usb.h>
+}
+
+#define BIT(i) (1<<(i))
+
+class LightsDriver_Linux_PacDrive: public LightsDriver
+{
+public:
+	LightsDriver_Linux_PacDrive();
+	~LightsDriver_Linux_PacDrive();
+
+	void Set( const LightsState *ls );
+private:
+
+	void FindDevice();
+	void OpenDevice();
+	void WriteDevice(uint16_t out);
+	void CloseDevice();
+
+	struct usb_device *Device;
+	usb_dev_handle *DeviceHandle;
+};
+
+#endif // LIGHTSDRIVER_LINUX_PACDRIVE_H
+
+/*
+ * Copyright (c) 2008 BoXoRRoXoRs
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, provided that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the Software and that both the above copyright notice(s) and this
+ * permission notice appear in supporting documentation.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
+ * THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS
+ * INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT
+ * OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
