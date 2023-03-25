870cfde5660b5e5151dd788c28bacebb103e842d
Martin Natano <natano@natano.net>
2023-03-25 23:02:32 +0100

    Fix git hash detection for release builds

--- Utils/build-release-linux.sh
+++ Utils/build-release-linux.sh
@@ -6,6 +6,7 @@ cd "$(dirname $0)"
 docker build -f Dockerfile-linux-amd64 . -t itgmania-linux-build:amd64
 
 docker run -i -v $(pwd)/..:/data itgmania-linux-build:amd64 sh -eux <<'EOF'
+git config --global --add safe.directory /data
 cmake -S /data -B /tmp/Build -DCMAKE_BUILD_TYPE=Release -DWITH_FULL_RELEASE=On -DWITH_CLUB_FANTASTIC=On
 cmake --build /tmp/Build -j $(nproc)
 cmake --build /tmp/Build --target package
