c62ab57e2555d674b086ea304b4e042f4b9774ba
teejusb <5017202+teejusb@users.noreply.github.com>
2023-04-05 12:19:58 -0700

    Update BPMDisplay to the Google Style Guide

--- src/BPMDisplay.h
+++ src/BPMDisplay.h
@@ -1,125 +1,100 @@
 #ifndef BPM_DISPLAY_H
 #define BPM_DISPLAY_H
 
-#include "BitmapText.h"
 #include "AutoActor.h"
-#include "ThemeMetric.h"
+#include "BitmapText.h"
+#include "Course.h"
+#include "GameConstantsAndTypes.h"
 #include "LocalizedString.h"
+#include "Song.h"
+#include "Steps.h"
+#include "ThemeMetric.h"
 
 #include <vector>
 
+// Displays a BPM or a range of BPMs.
+class BPMDisplay : public BitmapText {
+ public:
+  // Set up the BPM Display with default values.
+  BPMDisplay();
+  // Copy the BPMDisplay to another.
+  virtual BPMDisplay* Copy() const;
+  // Load the various metrics needed.
+  void Load();
+  // Update the display as required.
+  virtual void Update(float delta);
+  void LoadFromNode(const XNode* node);
+  // Use the BPM[s] from a song.
+  void SetBpmFromSong(const Song* song);
+  // Use the BPM[s] from a steps.
+  void SetBpmFromSteps(const Steps* steps);
+  // Use the BPM[s] from a course.
+  void SetBpmFromCourse(const Course* course);
+  // Use a specified, constant BPM.
+  void SetConstantBpm(float bpm);
+  // Have the BPMDisplay cycle between various BPMs.
+  void CycleRandomly();
+  // Don't use a BPM at all.
+  void NoBPM();
+  // Have the BPMDisplay use various BPMs.
+  void SetVarious();
+  // Have the GameState determine which BPMs to display.
+  void SetFromGameState();
 
-class Song;
-class Steps;
-class Course;
-struct DisplayBpms;
+  // Lua
+  virtual void PushSelf(lua_State* L);
 
-/** @brief Displays a BPM or a range of BPMs. */
-class BPMDisplay : public BitmapText
-{
-public:
-	/** @brief Set up the BPM Display with default values. */
-	BPMDisplay();
-	/** @brief Copy the BPMDisplay to another. */
-	virtual BPMDisplay *Copy() const;
-	/** @brief Load the various metrics needed. */
-	void Load();
-	/**
-	 * @brief Update the display as required.
-	 * @param fDeltaTime the changed time.
-	 */
-	virtual void Update( float fDeltaTime );
-	void LoadFromNode( const XNode *pNode );
-	/**
-	 * @brief Use the BPM[s] from a song.
-	 * @param pSong the song in question.
-	 */
-	void SetBpmFromSong( const Song* pSong );
-	/**
-	 * @brief Use the BPM[s] from a steps.
-	 * @param pSteps the steps in question.
-	 */
-	void SetBpmFromSteps( const Steps* pSteps );
-	/**
-	 * @brief Use the BPM[s] from a course.
-	 * @param pCourse the course in question.
-	 */
-	void SetBpmFromCourse( const Course* pCourse );
-	/**
-	 * @brief Use a specified, constant BPM.
-	 * @param fBPM the constant BPM.
-	 */
-	void SetConstantBpm( float fBPM );
-	/**
-	 * @brief Have the BPMDisplay cycle between various BPMs.
-	 */
-	void CycleRandomly();
-	/** @brief Don't use a BPM at all. */
-	void NoBPM();
-	/** @brief Have the BPMDisplay use various BPMs. */
-	void SetVarious();
-	/** @brief Have the GameState determine which BPMs to display. */
-	void SetFromGameState();
+ protected:
+  // Retrieve the active BPM on display.
+  float GetActiveBPM() const;
+  // Set the range to be used for the display.
+  void SetBPMRange(const DisplayBpms& display_bpms);
 
-	// Lua
-	virtual void PushSelf( lua_State *L );
+  // The commands to use when there is no BPM.
+  ThemeMetric<apActorCommands> SET_NO_BPM_COMMAND;
+  // The commands to use when there is a normal BPM.
+  ThemeMetric<apActorCommands> SET_NORMAL_COMMAND;
+  // The commands to use when the BPM can change between 2 or more values.
+  ThemeMetric<apActorCommands> SET_CHANGING_COMMAND;
+  // The commands to use when the BPM is random.
+  ThemeMetric<apActorCommands> SET_RANDOM_COMMAND;
+  // The commands to use if it is an extra stage.
+  ThemeMetric<apActorCommands> SET_EXTRA_COMMAND;
+  // A flag to determine if the BPMs cycle from low to high or just display
+  // both.
+  ThemeMetric<bool> CYCLE;
+  // A flag to determine if QUESTIONMARKS_TEXT is shown.
+  ThemeMetric<bool> SHOW_QMARKS;
+  // How often the random BPMs cycle themselves.
+  ThemeMetric<float> RANDOM_CYCLE_SPEED;
+  ThemeMetric<float> COURSE_CYCLE_SPEED;
+  // The text used to separate the low and high BPMs.
+  ThemeMetric<RString> SEPARATOR;
+  // The text used when there is no BPM.
+  ThemeMetric<RString> NO_BPM_TEXT;
+  // The text used when there are various BPMs for the song.
+  ThemeMetric<RString> VARIOUS_TEXT;
+  // The text used when it is a random BPM.
+  ThemeMetric<RString> RANDOM_TEXT;
+  // The text used as one possible option for random BPM.
+  ThemeMetric<RString> QUESTIONMARKS_TEXT;
+  // The format string used for the numbers.
+  ThemeMetric<RString> BPM_FORMAT_STRING;
 
-protected:
-	/**
-	 * @brief Retrieve the active BPM on display.
-	 * @return the active BPM on display.
-	 */
-	float GetActiveBPM() const;
-	/**
-	 * @brief Set the range to be used for the display.
-	 * @param bpms the set of BPMs to be used.
-	 */
-	void SetBPMRange( const DisplayBpms &bpms );
-
-	/** @brief The commands to use when there is no BPM. */
-	ThemeMetric<apActorCommands> SET_NO_BPM_COMMAND;
-	/** @brief The commands to use when there is a normal BPM. */
-	ThemeMetric<apActorCommands> SET_NORMAL_COMMAND;
-	/** @brief The commands to use when the BPM can change between 2 or more values. */
-	ThemeMetric<apActorCommands> SET_CHANGING_COMMAND;
-	/** @brief The commands to use when the BPM is random. */
-	ThemeMetric<apActorCommands> SET_RANDOM_COMMAND;
-	/** @brief The commands to use if it is an extra stage. */
-	ThemeMetric<apActorCommands> SET_EXTRA_COMMAND;
-	/** @brief A flag to determine if the BPMs cycle from low to high or just display both. */
-	ThemeMetric<bool> CYCLE;
-	/** @brief A flag to determine if QUESTIONMARKS_TEXT is shown. */
-	ThemeMetric<bool> SHOW_QMARKS;
-	/** @brief How often the random BPMs cycle themselves. */
-	ThemeMetric<float> RANDOM_CYCLE_SPEED;
-	ThemeMetric<float> COURSE_CYCLE_SPEED;
-	/** @brief The text used to separate the low and high BPMs. */
-	ThemeMetric<RString> SEPARATOR;
-	/** @brief The text used when there is no BPM. */
-	ThemeMetric<RString> NO_BPM_TEXT;
-	/** @brief The text used when there are various BPMs for the song. */
-	ThemeMetric<RString> VARIOUS_TEXT;
-	/** @brief The text used when it is a random BPM. */
-	ThemeMetric<RString> RANDOM_TEXT;
-	/** @brief The text used as one possible option for random BPM. */
-	ThemeMetric<RString> QUESTIONMARKS_TEXT;
-	/** @brief The format string used for the numbers. */
-	ThemeMetric<RString> BPM_FORMAT_STRING;
-
-	/** @brief The lowest valued BPM. */
-	float m_fBPMFrom;
-	/** @brief The highest valued BPM. */
-	float m_fBPMTo;
-	/** @brief The current BPM index used. */
-	int m_iCurrentBPM;
-	/** @brief The list of BPMs. */
-	std::vector<float> m_BPMS;
-	float m_fPercentInState;
-	/** @brief How long it takes to cycle the various BPMs. */
-	float m_fCycleTime;
+  // The lowest valued BPM.
+  float bpm_from_;
+  // The highest valued BPM.
+  float bpm_to_;
+  // The current BPM index used.
+  int current_bpm_;
+  // The list of BPMs.
+  std::vector<float> bpms_;
+  float percent_in_state_;
+  // How long it takes to cycle the various BPMs.
+  float cycle_time_;
 };
 
-#endif
+#endif  // BPM_DISPLAY_H
 
 /**
  * @file
