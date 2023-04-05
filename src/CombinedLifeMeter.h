007772783b6d46e00f610becb77cbfb7109331ad
teejusb <5017202+teejusb@users.noreply.github.com>
2023-04-05 13:21:14 -0700

    Update CombinedLifeMeter to the Google Style Guide

--- src/CombinedLifeMeter.h
+++ src/CombinedLifeMeter.h
@@ -1,46 +1,41 @@
 #ifndef COMBINEDLIFEMETER_H
 #define COMBINEDLIFEMETER_H
 
-#include "PlayerNumber.h"
-#include "GameConstantsAndTypes.h"
 #include "ActorFrame.h"
+#include "GameConstantsAndTypes.h"
+#include "PlayerNumber.h"
 
-/** @brief Multiple Players sharing one LifeMeter. */
-class CombinedLifeMeter : public ActorFrame
-{
-public:
-	CombinedLifeMeter() {};
-	virtual ~CombinedLifeMeter() {};
-	
-	virtual void OnLoadSong() {};
-	/**
-	 * @brief Change life after receiving a tap note grade.
-	 *
-	 * Note that this *is* called for the head of hold/roll notes.
-	 * @param pn the PlayerNumber that hit the TapNote.
-	 * @param tns the score received from the TapNote. */
-	virtual void ChangeLife( PlayerNumber pn, TapNoteScore tns ) = 0;
-	/**
-	 * @brief Change life after receiving a hold note grade.
-	 * @param pn the PlayerNumber that received the hold note score.
-	 * @param hns the hold note score recently received.
-	 * @param tns the score from the initial TapNote. */
-	virtual void ChangeLife( PlayerNumber pn, HoldNoteScore hns, TapNoteScore tns ) = 0;
-	virtual void ChangeLife(PlayerNumber pn, float delta) = 0;
-	virtual void SetLife(PlayerNumber pn, float value) = 0;
-	virtual void HandleTapScoreNone( PlayerNumber pn ) = 0;
+// Multiple Players sharing one LifeMeter.
+class CombinedLifeMeter : public ActorFrame {
+ public:
+  CombinedLifeMeter(){};
+  virtual ~CombinedLifeMeter(){};
+
+  virtual void OnLoadSong(){};
+  // Change life after receiving a tap note grade.
+  // Note that this *is* called for the head of hold/roll notes.
+  // - pn, the PlayerNumber that hit the TapNote.
+  // - tns, the score received from the TapNote.
+  virtual void ChangeLife(PlayerNumber pn, TapNoteScore tns) = 0;
+  // Change life after receiving a hold note grade.
+  // - pn, the PlayerNumber that received the hold note score.
+  // - hns, the hold note score recently received.
+  // - tns, the score from the initial TapNote.
+  virtual void ChangeLife(
+      PlayerNumber pn, HoldNoteScore hns, TapNoteScore tns) = 0;
+  virtual void ChangeLife(PlayerNumber pn, float delta) = 0;
+  virtual void SetLife(PlayerNumber pn, float value) = 0;
+  virtual void HandleTapScoreNone(PlayerNumber pn) = 0;
 };
 
-
-
-#endif
+#endif  // COMBINEDLIFEMETER_H
 
 /**
  * @file
  * @author Chris Danford (c) 2003
  * @section LICENSE
  * All rights reserved.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -50,7 +45,7 @@ public:
  * copyright notice(s) and this permission notice appear in all copies of
  * the Software and that both the above copyright notice(s) and this
  * permission notice appear in supporting documentation.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
