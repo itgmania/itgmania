9890fb09dad887588181b93346ce4277e46361a3
teejusb <5017202+teejusb@users.noreply.github.com>
2023-04-27 15:20:18 -0700

    Update CommonMetrics to the Google Style Guide

--- src/CommonMetrics.h
+++ src/CommonMetrics.h
@@ -1,87 +1,87 @@
 #ifndef COMMON_METRICS_H
 #define COMMON_METRICS_H
 
-#include "ThemeMetric.h"
-#include "PlayerNumber.h"
+#include <vector>
+
 #include "Difficulty.h"
 #include "GameConstantsAndTypes.h"
 #include "LocalizedString.h"
-
-#include <vector>
-
+#include "PlayerNumber.h"
+#include "ThemeMetric.h"
 
 // Types
-class ThemeMetricDifficultiesToShow : public ThemeMetric<RString>
-{
-public:
-	ThemeMetricDifficultiesToShow(): m_v() { }
-	ThemeMetricDifficultiesToShow( const RString& sGroup, const RString& sName );
-	void Read();
-	const std::vector<Difficulty> &GetValue() const;
-private:
-	std::vector<Difficulty> m_v;
-};
-class ThemeMetricCourseDifficultiesToShow : public ThemeMetric<RString>
-{
-public:
-	ThemeMetricCourseDifficultiesToShow(): m_v() { }
-	ThemeMetricCourseDifficultiesToShow( const RString& sGroup, const RString& sName );
-	void Read();
-	const std::vector<CourseDifficulty> &GetValue() const;
-private:
-	std::vector<CourseDifficulty> m_v;
-};
-class ThemeMetricStepsTypesToShow : public ThemeMetric<RString>
-{
-public:
-	ThemeMetricStepsTypesToShow(): m_v() { }
-	ThemeMetricStepsTypesToShow( const RString& sGroup, const RString& sName );
-	void Read();
-	const std::vector<StepsType> &GetValue() const;
-private:
-	std::vector<StepsType> m_v;
+class ThemeMetricDifficultiesToShow : public ThemeMetric<RString> {
+ public:
+  ThemeMetricDifficultiesToShow() : difficulties_() {}
+  ThemeMetricDifficultiesToShow(const RString& group, const RString& name);
+  void Read();
+  const std::vector<Difficulty>& GetValue() const;
+
+ private:
+  std::vector<Difficulty> difficulties_;
 };
 
+class ThemeMetricCourseDifficultiesToShow : public ThemeMetric<RString> {
+ public:
+  ThemeMetricCourseDifficultiesToShow() : course_difficulties_() {}
+  ThemeMetricCourseDifficultiesToShow(
+      const RString& group, const RString& name);
+  void Read();
+  const std::vector<CourseDifficulty>& GetValue() const;
 
-/**
- * @brief Definitions of metrics that are in the "Common" group.
- *
- * These metrics are used throughout the metrics file. */
-namespace CommonMetrics
-{
-	/** @brief The first screen in the attract loop. */
-	extern ThemeMetric<RString>		FIRST_ATTRACT_SCREEN;
-	/** @brief The screen that appears when pressing the operator button. */
-	extern ThemeMetric<RString>		OPERATOR_MENU_SCREEN;
-	/** @brief The default modifiers to apply. */
-	extern ThemeMetric<RString>		DEFAULT_MODIFIERS;
-	/** @brief The caption on the title bar. */
-	extern LocalizedString				WINDOW_TITLE;
-	/** @brief How many entries should be shown before showing "Various" instead. */
-	extern ThemeMetric<int>			MAX_COURSE_ENTRIES_BEFORE_VARIOUS;
-	/** @brief Adjusts the assist tick sound's playback time. */
-	extern ThemeMetric<float>			TICK_EARLY_SECONDS;
-	/** @brief the name of the default noteskin. */
-	extern ThemeMetric<RString>		DEFAULT_NOTESKIN_NAME;
-	/** @brief Which difficulties are to be shown? */
-	extern ThemeMetricDifficultiesToShow	DIFFICULTIES_TO_SHOW;
-	/** @brief Which course difficulties are to be shown? */
-	extern ThemeMetricCourseDifficultiesToShow	COURSE_DIFFICULTIES_TO_SHOW;
-	/**
-	 * @brief Which step types are to be shown?
-	 *
-	 * This metric (StepsTypesToHide) takes a list of StepsTypes to hide and
-	 * returns a list of StepsTypes to show. */
-	extern ThemeMetricStepsTypesToShow	STEPS_TYPES_TO_SHOW;
-	/** @brief Does the player need to explicitly set a style? */
-	extern ThemeMetric<bool>			AUTO_SET_STYLE;
-	/** @brief How many decimal places are used? */
-	extern ThemeMetric<int>			PERCENT_SCORE_DECIMAL_PLACES;
-
-	extern ThemeMetric<RString>		IMAGES_TO_CACHE;
-
-	RString LocalizeOptionItem( const RString &s, bool bOptional );
+ private:
+  std::vector<CourseDifficulty> course_difficulties_;
 };
+
+class ThemeMetricStepsTypesToShow : public ThemeMetric<RString> {
+ public:
+  ThemeMetricStepsTypesToShow() : steps_types_() {}
+  ThemeMetricStepsTypesToShow(const RString& group, const RString& name);
+  void Read();
+  const std::vector<StepsType>& GetValue() const;
+
+ private:
+  std::vector<StepsType> steps_types_;
+};
+
+// Definitions of metrics that are in the "Common" group.
+//
+// These metrics are used throughout the metrics file.
+namespace CommonMetrics {
+
+// The first screen in the attract loop.
+extern ThemeMetric<RString> FIRST_ATTRACT_SCREEN;
+// The screen that appears when pressing the operator button.
+extern ThemeMetric<RString> OPERATOR_MENU_SCREEN;
+// The default modifiers to apply.
+extern ThemeMetric<RString> DEFAULT_MODIFIERS;
+// The caption on the title bar.
+extern LocalizedString WINDOW_TITLE;
+// How many entries should be shown before showing "Various" instead.
+extern ThemeMetric<int> MAX_COURSE_ENTRIES_BEFORE_VARIOUS;
+// Adjusts the assist tick sound's playback time.
+extern ThemeMetric<float> TICK_EARLY_SECONDS;
+// the name of the default noteskin.
+extern ThemeMetric<RString> DEFAULT_NOTESKIN_NAME;
+// Which difficulties are to be shown?
+extern ThemeMetricDifficultiesToShow DIFFICULTIES_TO_SHOW;
+// Which course difficulties are to be shown?
+extern ThemeMetricCourseDifficultiesToShow COURSE_DIFFICULTIES_TO_SHOW;
+// Which step types are to be shown?
+//
+// This metric (StepsTypesToHide) takes a list of StepsTypes to hide and
+// returns a list of StepsTypes to show.
+extern ThemeMetricStepsTypesToShow STEPS_TYPES_TO_SHOW;
+// Does the player need to explicitly set a style?
+extern ThemeMetric<bool> AUTO_SET_STYLE;
+// How many decimal places are used?
+extern ThemeMetric<int> PERCENT_SCORE_DECIMAL_PLACES;
+
+extern ThemeMetric<RString> IMAGES_TO_CACHE;
+
+RString LocalizeOptionItem(const RString& option, bool is_optional);
+
+};  // namespace CommonMetrics
 
 #endif
 
