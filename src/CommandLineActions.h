246ad72df5022e89815a4fc5598d7b0dd28fa5bf
teejusb <5017202+teejusb@users.noreply.github.com>
2023-04-27 15:07:31 -0700

    Update CommandLineActions to the Google Style Guide

--- src/CommandLineActions.h
+++ src/CommandLineActions.h
@@ -3,30 +3,28 @@
 
 #include <vector>
 
+#include "LoadingWindow.h"
 
-class LoadingWindow;
+// The collection of command line actions.
+namespace CommandLineActions {
 
-/** @brief The collection of command line actions. */
-namespace CommandLineActions
-{
-	/**
-	 * @brief Perform a utility function, then exit.
-	 * @param pLW the LoadingWindow that is presently not used? */
-	void Handle(LoadingWindow* pLW);
+// Perform a utility function, then exit.
+// pLW the LoadingWindow that is presently not used?
+void Handle(LoadingWindow* pLW);
 
-	/** @brief The housing for the command line arguments. */
-	class CommandLineArgs
-	{
-	public:
-		/** @brief the arguments in question. */
-		std::vector<RString> argv;
-	};
-	/**
-	 * @brief A list of command line arguemnts to process while the game is running.
-	 * These args could have come from this process or passed to this process
-	 * from another process. */
-	extern std::vector<CommandLineArgs> ToProcess;
-}
+// The housing for the command line arguments.
+class CommandLineArgs {
+ public:
+  // the arguments in question.
+  std::vector<RString> argv;
+};
+
+// A list of command line arguemnts to process while the game is running.
+// These args could have come from this process or passed to this process
+// from another process.
+extern std::vector<CommandLineArgs> ToProcess;
+
+}  // namespace CommandLineActions
 
 #endif
 
@@ -56,4 +54,3 @@ namespace CommandLineActions
  * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  * PERFORMANCE OF THIS SOFTWARE.
  */
-
