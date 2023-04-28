ebfa9079c0e9df29a69924e2223db02481a6d921
teejusb <5017202+teejusb@users.noreply.github.com>
2023-04-28 11:09:10 -0700

    Update CourseWriterCRS to the Google Style Guide

--- src/CourseWriterCRS.h
+++ src/CourseWriterCRS.h
@@ -1,51 +1,31 @@
-/** @brief CourseWriterCRS - Writes a Course to an .CRS file. */
-
 #ifndef COURSE_WRITER_CRS_H
 #define COURSE_WRITER_CRS_H
 
-class Course;
-class RageFileBasic;
+#include "Course.h"
+#include "RageFileBasic.h"
 
-/** @brief The Course Writer handles writing the .crs files. */
-namespace CourseWriterCRS
-{
-	/**
-	 * @brief Write the course to a file.
-	 * @param course the course contents.
-	 * @param f the file being built.
-	 * @param bSavingCache is true if cache information is being saved as well.
-	 * @return its success or failure.
-	 */
-	bool Write( const Course &course, RageFileBasic &f, bool bSavingCache );
-	/**
-	 * @brief Write the course to a file.
-	 * @param course the course contents.
-	 * @param sPath the path to the file.
-	 * @param bSavingCache is true if cache information is being saved as well.
-	 * @return its success or failure.
-	 */
-	bool Write( const Course &course, const RString &sPath, bool bSavingCache );
-	/**
-	 * @brief Retrieve course information from a file for eventual writing.
-	 * @param pCourse the course file.
-	 * @param sOut the path to the file.
-	 */
-	void GetEditFileContents( const Course *pCourse, RString &sOut );
-	/**
-	 * @brief Write the custom course to the machine's hard drive.
-	 * @param pCourse the course file.
-	 */
-	void WriteEditFileToMachine( const Course *pCourse );
-}
+// The Course Writer handles writing a course to a .crs files.
+namespace CourseWriterCRS {
 
-#endif
+// Write the course to a file.
+bool Write(const Course& course, RageFileBasic& f, bool saving_cache);
+// Write the course to a file.
+bool Write(const Course& course, const RString& path, bool saving_cache);
+// Retrieve course information from a file for eventual writing.
+void GetEditFileContents(const Course* course, RString& out);
+// Write the custom course to the machine's hard drive.
+void WriteEditFileToMachine(const Course* course);
+
+}  // namespace CourseWriterCRS
+
+#endif  // COURSE_WRITER_CRS_H
 
 /**
  * @file
  * @author Chris Danford (c) 2001-2005
  * @section LICENSE
  * All rights reserved.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -55,7 +35,7 @@ namespace CourseWriterCRS
  * copyright notice(s) and this permission notice appear in all copies of
  * the Software and that both the above copyright notice(s) and this
  * permission notice appear in supporting documentation.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
