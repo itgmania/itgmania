fadb1ca3440c9e5d64de652aa0f393ce6ec78619
teejusb <5017202+teejusb@users.noreply.github.com>
2023-04-29 00:07:59 -0700

    Update Difficulty to the Google Style Guide

--- src/Difficulty.cpp
+++ src/Difficulty.cpp
@@ -1,169 +1,167 @@
 #include "global.h"
+
 #include "Difficulty.h"
-#include "GameState.h"
-#include "ThemeMetric.h"
-#include "LuaManager.h"
-#include "LocalizedString.h"
-#include "GameConstantsAndTypes.h"
-#include "GameManager.h"
-#include "Steps.h"
-#include "Trail.h"
 
 #include <vector>
 
+#include "GameConstantsAndTypes.h"
+#include "GameManager.h"
+#include "GameState.h"
+#include "LocalizedString.h"
+#include "LuaManager.h"
+#include "Steps.h"
+#include "ThemeMetric.h"
+#include "Trail.h"
 
-static const char *DifficultyNames[] = {
-	"Beginner",
-	"Easy",
-	"Medium",
-	"Hard",
-	"Challenge",
-	"Edit",
+static const char* DifficultyNames[] = {
+    "Beginner", "Easy", "Medium", "Hard", "Challenge", "Edit",
 };
-XToString( Difficulty );
-StringToX( Difficulty );
-LuaXType( Difficulty );
+XToString(Difficulty);
+StringToX(Difficulty);
+LuaXType(Difficulty);
 
-const RString &CourseDifficultyToLocalizedString( CourseDifficulty x )
-{
-	static std::unique_ptr<LocalizedString> g_CourseDifficultyName[NUM_Difficulty];
-	if( g_CourseDifficultyName[0].get() == nullptr )
-	{
-		FOREACH_ENUM( Difficulty,i)
-		{
-			std::unique_ptr<LocalizedString> ap( new LocalizedString("CourseDifficulty", DifficultyToString(i)) );
-			g_CourseDifficultyName[i] = move(ap);
-		}
-	}
-	return g_CourseDifficultyName[x]->GetValue();
+const RString& CourseDifficultyToLocalizedString(
+    CourseDifficulty course_difficulty) {
+  static std::unique_ptr<LocalizedString>
+      g_CourseDifficultyName[NUM_Difficulty];
+  if (g_CourseDifficultyName[0].get() == nullptr) {
+    FOREACH_ENUM(Difficulty, i) {
+      std::unique_ptr<LocalizedString> ap(
+          new LocalizedString("CourseDifficulty", DifficultyToString(i)));
+      g_CourseDifficultyName[i] = move(ap);
+    }
+  }
+  return g_CourseDifficultyName[course_difficulty]->GetValue();
 }
 
-LuaFunction( CourseDifficultyToLocalizedString, CourseDifficultyToLocalizedString(Enum::Check<Difficulty>(L, 1)) );
+LuaFunction(
+    CourseDifficultyToLocalizedString,
+    CourseDifficultyToLocalizedString(Enum::Check<Difficulty>(L, 1)));
 
-CourseDifficulty GetNextShownCourseDifficulty( CourseDifficulty cd )
-{
-	for( CourseDifficulty d=(CourseDifficulty)(cd+1); d<NUM_Difficulty; enum_add(d, 1) )
-	{
-		if( GAMESTATE->IsCourseDifficultyShown(d) )
-			return d;
-	}
-	return Difficulty_Invalid;
+CourseDifficulty GetNextShownCourseDifficulty(
+    CourseDifficulty course_difficulty) {
+  for (CourseDifficulty d = (CourseDifficulty)(course_difficulty + 1);
+       d < NUM_Difficulty; enum_add(d, 1)) {
+    if (GAMESTATE->IsCourseDifficultyShown(d)) {
+      return d;
+    }
+  }
+  return Difficulty_Invalid;
 }
 
-struct OldStyleStringToDifficultyMapHolder
-{
-	std::map<RString, Difficulty> conversion_map;
-	OldStyleStringToDifficultyMapHolder()
-	{
-		conversion_map["beginner"]= Difficulty_Beginner;
-		conversion_map["easy"]= Difficulty_Easy;
-		conversion_map["basic"]= Difficulty_Easy;
-		conversion_map["light"]= Difficulty_Easy;
-		conversion_map["medium"]= Difficulty_Medium;
-		conversion_map["another"]= Difficulty_Medium;
-		conversion_map["trick"]= Difficulty_Medium;
-		conversion_map["standard"]= Difficulty_Medium;
-		conversion_map["difficult"]= Difficulty_Medium;
-		conversion_map["hard"]= Difficulty_Hard;
-		conversion_map["ssr"]= Difficulty_Hard;
-		conversion_map["maniac"]= Difficulty_Hard;
-		conversion_map["heavy"]= Difficulty_Hard;
-		conversion_map["smaniac"]= Difficulty_Challenge;
-		conversion_map["challenge"]= Difficulty_Challenge;
-		conversion_map["expert"]= Difficulty_Challenge;
-		conversion_map["oni"]= Difficulty_Challenge;
-		conversion_map["edit"]= Difficulty_Edit;
-	}
+struct OldStyleStringToDifficultyMapHolder {
+  std::map<RString, Difficulty> conversion_map;
+  OldStyleStringToDifficultyMapHolder() {
+    conversion_map["beginner"] = Difficulty_Beginner;
+    conversion_map["easy"] = Difficulty_Easy;
+    conversion_map["basic"] = Difficulty_Easy;
+    conversion_map["light"] = Difficulty_Easy;
+    conversion_map["medium"] = Difficulty_Medium;
+    conversion_map["another"] = Difficulty_Medium;
+    conversion_map["trick"] = Difficulty_Medium;
+    conversion_map["standard"] = Difficulty_Medium;
+    conversion_map["difficult"] = Difficulty_Medium;
+    conversion_map["hard"] = Difficulty_Hard;
+    conversion_map["ssr"] = Difficulty_Hard;
+    conversion_map["maniac"] = Difficulty_Hard;
+    conversion_map["heavy"] = Difficulty_Hard;
+    conversion_map["smaniac"] = Difficulty_Challenge;
+    conversion_map["challenge"] = Difficulty_Challenge;
+    conversion_map["expert"] = Difficulty_Challenge;
+    conversion_map["oni"] = Difficulty_Challenge;
+    conversion_map["edit"] = Difficulty_Edit;
+  }
 };
 OldStyleStringToDifficultyMapHolder OldStyleStringToDifficulty_converter;
-Difficulty OldStyleStringToDifficulty( const RString& sDC )
-{
-	RString s2 = sDC;
-	s2.MakeLower();
-	std::map<RString, Difficulty>::iterator diff=
-		OldStyleStringToDifficulty_converter.conversion_map.find(s2);
-	if(diff != OldStyleStringToDifficulty_converter.conversion_map.end())
-	{
-		return diff->second;
-	}
-	return Difficulty_Invalid;
+Difficulty OldStyleStringToDifficulty(const RString& difficulty_str) {
+  RString s2 = difficulty_str;
+  s2.MakeLower();
+  auto diff = OldStyleStringToDifficulty_converter.conversion_map.find(s2);
+  if (diff != OldStyleStringToDifficulty_converter.conversion_map.end()) {
+    return diff->second;
+  }
+  return Difficulty_Invalid;
 }
 
-LuaFunction( OldStyleStringToDifficulty, OldStyleStringToDifficulty(SArg(1)) );
+LuaFunction(OldStyleStringToDifficulty, OldStyleStringToDifficulty(SArg(1)));
 
-static ThemeMetric<RString> NAMES("CustomDifficulty","Names");
+static ThemeMetric<RString> NAMES("CustomDifficulty", "Names");
 
-RString GetCustomDifficulty( StepsType st, Difficulty dc, CourseType ct )
-{
-	/* XXX GAMEMAN->GetStepsTypeInfo( StepsType_Invalid ) will crash. I'm not
-	 * sure what the correct behavior in this case should be. Should we still
-	 * allow custom difficulties? Why do we not allow custom difficulties for
-	 * Edit? - Steve */
-	// CustomDifficulty for Edit defeats the purpose of the edit's name. -aj
-	if( st == StepsType_Invalid )
-	{
-		/* This is not totally necessary since DifficultyToString() will
-		 * return "", but the comment there says that the caller should
-		 * really be checking for invalid values. */
-		if( dc == Difficulty_Invalid )
-			return RString();
-		return DifficultyToString( dc );
-	}
+RString GetCustomDifficulty(
+    StepsType steps_type, Difficulty difficulty, CourseType course_type) {
+  // NOTE(Steve): GAMEMAN->GetStepsTypeInfo( StepsType_Invalid ) will crash.
+  // I'm not sure what the correct behavior in this case should be. Should we
+  // still allow custom difficulties? Why do we not allow custom difficulties for
+  // Edit?
+  // NOTE(aj): CustomDifficulty for Edit defeats the purpose of the edit's name.
+  if (steps_type == StepsType_Invalid) {
+    // This is not totally necessary since DifficultyToString() will
+    // return "", but the comment there says that the caller should
+    // really be checking for invalid values.
+    if (difficulty == Difficulty_Invalid) {
+      return RString();
+    }
+    return DifficultyToString(difficulty);
+  }
 
-	if( dc == Difficulty_Edit )
-	{
-		return "Edit";
-	}
-	// OPTIMIZATION OPPORTUNITY: cache these metrics and cache the splitting
-	std::vector<RString> vsNames;
-	split( NAMES, ",", vsNames );
-	for (RString const &sName: vsNames)
-	{
-		ThemeMetric<StepsType> STEPS_TYPE("CustomDifficulty",sName + "StepsType");
-		if( STEPS_TYPE == StepsType_Invalid  ||  st == STEPS_TYPE )	// match
-		{
-			ThemeMetric<Difficulty> DIFFICULTY("CustomDifficulty",sName + "Difficulty");
-			if( DIFFICULTY == Difficulty_Invalid  ||  dc == DIFFICULTY )	// match
-			{
-				ThemeMetric<CourseType> COURSE_TYPE("CustomDifficulty",sName + "CourseType");
-				if( COURSE_TYPE == CourseType_Invalid  ||  ct == COURSE_TYPE )	// match
-				{
-					ThemeMetric<RString> STRING("CustomDifficulty",sName + "String");
-					return STRING.GetValue();
-				}
-			}
-		}
-	}
-	// no matching CustomDifficulty, so use a regular difficulty name
-	if( dc == Difficulty_Invalid )
-		return RString();
-	return DifficultyToString( dc );
+  if (difficulty == Difficulty_Edit) {
+    return "Edit";
+  }
+  // OPTIMIZATION OPPORTUNITY: cache these metrics and cache the splitting
+  std::vector<RString> names;
+  split(NAMES, ",", names);
+  for (const RString& name : names) {
+    ThemeMetric<StepsType> STEPS_TYPE("CustomDifficulty", name + "StepsType");
+    if (STEPS_TYPE == StepsType_Invalid || steps_type == STEPS_TYPE) {
+      ThemeMetric<Difficulty> DIFFICULTY(
+          "CustomDifficulty", name + "Difficulty");
+      if (DIFFICULTY == Difficulty_Invalid || difficulty == DIFFICULTY) {
+        ThemeMetric<CourseType> COURSE_TYPE(
+            "CustomDifficulty", name + "CourseType");
+        if (COURSE_TYPE == CourseType_Invalid || course_type == COURSE_TYPE) {
+          ThemeMetric<RString> STRING("CustomDifficulty", name + "String");
+          return STRING.GetValue();
+        }
+      }
+    }
+  }
+  // no matching CustomDifficulty, so use a regular difficulty name
+  if (difficulty == Difficulty_Invalid) {
+    return RString();
+  }
+  return DifficultyToString(difficulty);
 }
 
-LuaFunction( GetCustomDifficulty, GetCustomDifficulty(Enum::Check<StepsType>(L,1), Enum::Check<Difficulty>(L, 2), Enum::Check<CourseType>(L, 3, true)) );
+LuaFunction(
+    GetCustomDifficulty,
+    GetCustomDifficulty(
+        Enum::Check<StepsType>(L, 1), Enum::Check<Difficulty>(L, 2),
+        Enum::Check<CourseType>(L, 3, true)));
 
-RString CustomDifficultyToLocalizedString( const RString &sCustomDifficulty )
-{
-	return THEME->GetString( "CustomDifficulty", sCustomDifficulty );
+RString CustomDifficultyToLocalizedString(const RString& custom_difficulty) {
+  return THEME->GetString("CustomDifficulty", custom_difficulty);
 }
 
-LuaFunction( CustomDifficultyToLocalizedString, CustomDifficultyToLocalizedString(SArg(1)) );
+LuaFunction(
+    CustomDifficultyToLocalizedString,
+    CustomDifficultyToLocalizedString(SArg(1)));
 
-
-RString StepsToCustomDifficulty( const Steps *pSteps )
-{
-	return GetCustomDifficulty( pSteps->m_StepsType, pSteps->GetDifficulty(), CourseType_Invalid );
+RString StepsToCustomDifficulty(const Steps* steps) {
+  return GetCustomDifficulty(
+      steps->m_StepsType, steps->GetDifficulty(), CourseType_Invalid);
 }
 
-RString TrailToCustomDifficulty( const Trail *pTrail )
-{
-	return GetCustomDifficulty( pTrail->m_StepsType, pTrail->m_CourseDifficulty, pTrail->m_CourseType );
+RString TrailToCustomDifficulty(const Trail* trail) {
+  return GetCustomDifficulty(
+      trail->m_StepsType, trail->m_CourseDifficulty, trail->m_CourseType);
 }
 
 #include "LuaBinding.h"
 
-LuaFunction( StepsToCustomDifficulty, StepsToCustomDifficulty(Luna<Steps>::check(L, 1)) );
-LuaFunction( TrailToCustomDifficulty, TrailToCustomDifficulty(Luna<Trail>::check(L, 1)) );
+LuaFunction(
+    StepsToCustomDifficulty, StepsToCustomDifficulty(Luna<Steps>::check(L, 1)));
+LuaFunction(
+    TrailToCustomDifficulty, TrailToCustomDifficulty(Luna<Trail>::check(L, 1)));
 
 /*
  * (c) 2001-2004 Chris Danford
--- src/Difficulty.h
+++ src/Difficulty.h
@@ -3,53 +3,57 @@
 
 #include "EnumHelper.h"
 #include "GameConstantsAndTypes.h"
+
+// TODO(teejusb): Remove forward declarations. Currently, the following
+// both make use of Difficulty.
 class Steps;
 class Trail;
 
 // Player number stuff
-enum Difficulty
-{
-	Difficulty_Beginner,
-	Difficulty_Easy,
-	Difficulty_Medium,
-	Difficulty_Hard,
-	Difficulty_Challenge,
-	Difficulty_Edit,
-	NUM_Difficulty,
-	Difficulty_Invalid
+enum Difficulty {
+  Difficulty_Beginner,
+  Difficulty_Easy,
+  Difficulty_Medium,
+  Difficulty_Hard,
+  Difficulty_Challenge,
+  Difficulty_Edit,
+  NUM_Difficulty,
+  Difficulty_Invalid
 };
-const RString& DifficultyToString( Difficulty dc );
-Difficulty StringToDifficulty( const RString& sDC );
-LuaDeclareType( Difficulty );
+const RString& DifficultyToString(Difficulty dc);
+Difficulty StringToDifficulty(const RString& sDC);
+LuaDeclareType(Difficulty);
 
-Difficulty OldStyleStringToDifficulty( const RString& sDC ); // compatibility
+// Compatibility.
+Difficulty OldStyleStringToDifficulty(const RString& difficulty_str);
 
 typedef Difficulty CourseDifficulty;
 const int NUM_CourseDifficulty = NUM_Difficulty;
-/** @brief Loop through the shown course difficulties. */
-#define FOREACH_ShownCourseDifficulty( cd ) \
-for( Difficulty cd=GetNextShownCourseDifficulty((CourseDifficulty)-1); \
-	cd!=Difficulty_Invalid; cd=GetNextShownCourseDifficulty(cd) )
+// Loop through the shown course difficulties.
+#define FOREACH_ShownCourseDifficulty(cd)                                  \
+  for (Difficulty cd = GetNextShownCourseDifficulty((CourseDifficulty)-1); \
+       cd != Difficulty_Invalid; cd = GetNextShownCourseDifficulty(cd))
 
-const RString& CourseDifficultyToLocalizedString( Difficulty dc );
+const RString& CourseDifficultyToLocalizedString(
+    CourseDifficulty course_difficulty);
 
-Difficulty GetNextShownCourseDifficulty( Difficulty pn );
+Difficulty GetNextShownCourseDifficulty(CourseDifficulty course_difficulty);
 
+// CustomDifficulty is a themeable difficulty name based on Difficulty, string
+// matching on StepsType, and CourseType. It is used to look up localized
+// strings and look up colors.
+RString GetCustomDifficulty(
+    StepsType steps_type, Difficulty difficulty, CourseType course_type);
+RString CustomDifficultyToLocalizedString(const RString& custom_difficulty);
+RString StepsToCustomDifficulty(const Steps* steps);
+RString TrailToCustomDifficulty(const Trail* trail);
 
-// CustomDifficulty is a themeable difficulty name based on Difficulty, string matching on StepsType, and CourseType.
-// It is used to look up localized strings and look up colors.
-RString GetCustomDifficulty( StepsType st, Difficulty dc, CourseType ct );
-RString CustomDifficultyToLocalizedString( const RString &sCustomDifficulty );
-RString StepsToCustomDifficulty( const Steps *pSteps );
-RString TrailToCustomDifficulty( const Trail *pTrail );
-
-
-#endif
+#endif  // DIFFICULTY_H
 
 /*
  * (c) 2001-2004 Chris Danford
  * All rights reserved.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -59,7 +63,7 @@ RString TrailToCustomDifficulty( const Trail *pTrail );
  * copyright notice(s) and this permission notice appear in all copies of
  * the Software and that both the above copyright notice(s) and this
  * permission notice appear in supporting documentation.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
