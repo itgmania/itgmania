e1a0da9cd361d6525b30491bf8675f73a5783817
teejusb <5017202+teejusb@users.noreply.github.com>
2023-04-29 14:34:08 -0700

    Update DisplayResolutions.h
    Update DisplayResolutions to the Google Style Guide

--- src/DisplayResolutions.h
+++ src/DisplayResolutions.h
@@ -2,43 +2,42 @@
 #define DisplayResolutions_H
 
 #include <set>
-/** @brief The dimensions of the program. */
-class DisplayResolution
-{
-public:
-	/** @brief The width of the program. */
-	int iWidth;
-	/** @brief The height of the program. */
-	int iHeight;
-	/** @brief Is this display stretched/used for widescreen? */
-	bool bStretched;
 
-	/**
-	 * @brief Determine if one DisplayResolution is less than the other.
-	 * @param other the other DisplayResolution to check.
-	 * @return true if this DisplayResolution is less than the other, or false otherwise. */
-	bool operator<( const DisplayResolution &other ) const
-	{
-/** @brief A quick way to compare the two DisplayResolutions. */
-#define COMPARE(x) if( x != other.x ) return x < other.x;
-		COMPARE( iWidth );
-		COMPARE( iHeight );
-		COMPARE( bStretched );
+// The dimensions of the program.
+class DisplayResolution {
+ public:
+  // The width of the program.
+  int width_;
+  // The height of the program.
+  int height_;
+  // Is this display stretched/used for widescreen?
+  bool is_stretched_;
+
+  // Determine if one DisplayResolution is less than the other.
+  bool operator<(const DisplayResolution& other) const {
+
+// A quick way to compare the two DisplayResolutions.
+#define COMPARE(x) \
+  if (x != other.x) return x < other.x;
+    COMPARE(width_);
+    COMPARE(height_);
+    COMPARE(is_stretched_);
 #undef COMPARE
-		return false;
-	}
+    return false;
+  }
 };
-/** @brief The collection of DisplayResolutions available within the program. */
+
+// The collection of DisplayResolutions available within the program.
 typedef std::set<DisplayResolution> DisplayResolutions;
 
-#endif
+#endif  // DisplayResolutions_H
 
 /**
  * @file
  * @author Chris Danford (c) 2001-2005
  * @section LICENSE
  * All rights reserved.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -48,7 +47,7 @@ typedef std::set<DisplayResolution> DisplayResolutions;
  * copyright notice(s) and this permission notice appear in all copies of
  * the Software and that both the above copyright notice(s) and this
  * permission notice appear in supporting documentation.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
