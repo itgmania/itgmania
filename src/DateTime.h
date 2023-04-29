df04ae1f82a2da53b79f4f03bc9da849617f91b9
teejusb <5017202+teejusb@users.noreply.github.com>
2023-04-28 18:54:11 -0700

    Update DateTime to the Google Style Guide

--- src/DateTime.h
+++ src/DateTime.h
@@ -1,161 +1,118 @@
 #ifndef DATE_TIME_H
 #define DATE_TIME_H
 
-#include "EnumHelper.h"
 #include <ctime>
 
-int StringToDayInYear( RString sDayInYear );
+#include "EnumHelper.h"
 
-/** @brief The number of days we check for previously. */
+int StringToDayInYear(RString sDayInYear);
+
+// The number of days we check for previously.
 const int NUM_LAST_DAYS = 7;
-/** @brief The number of weeks we check for previously. */
+// The number of weeks we check for previously.
 const int NUM_LAST_WEEKS = 52;
-/**
- * @brief The number of days that are in a year.
- *
- * This is set up to be a maximum for leap years. */
+// The number of days that are in a year.
+// This is set up to be a maximum for leap years.
 const int DAYS_IN_YEAR = 366;
-/**
- * @brief The number of hours in a day. */
+// The number of hours in a day.
 const int HOURS_IN_DAY = 24;
-/**
- * @brief The number of days that are in a week. */
+// The number of days that are in a week.
 const int DAYS_IN_WEEK = 7;
-/** @brief Which month are we focusing on?
- *
- * Is there any reason why the actual months aren't defined 
- * in here? -Wolfman2000 */
-enum Month 
-{ 
-	NUM_Month = 12, /**< The number of months in the year. */
-	Month_Invalid /**< There should be no month at this point. */
+// Which month are we focusing on?
+//
+// NOTE(Wolfman2000):Is there any reason why the actual months aren't defined
+// in here?
+enum Month {
+	// The number of months in the year.
+  NUM_Month = 12,
+	// There should be no month at this point.
+  Month_Invalid
 };
 
-RString DayInYearToString( int iDayInYearIndex );
-RString LastDayToString( int iLastDayIndex );
-RString LastDayToLocalizedString( int iLastDayIndex );
-RString DayOfWeekToString( int iDayOfWeekIndex );
-RString DayOfWeekToLocalizedString( int iDayOfWeekIndex );
-RString HourInDayToString( int iHourIndex );
-RString HourInDayToLocalizedString( int iHourIndex );
-const RString &MonthToString( Month month );
-const RString &MonthToLocalizedString( Month month );
-RString LastWeekToString( int iLastWeekIndex );
-RString LastWeekToLocalizedString( int iLastWeekIndex );
-LuaDeclareType( Month );
+RString DayInYearToString(int day_in_year_index);
+RString LastDayToString(int last_day_index);
+RString LastDayToLocalizedString(int last_day_index);
+RString DayOfWeekToString(int day_of_week_index);
+RString DayOfWeekToLocalizedString(int day_of_week_index);
+RString HourInDayToString(int hour_index);
+RString HourInDayToLocalizedString(int hour_index);
+const RString& MonthToString(Month month);
+const RString& MonthToLocalizedString(Month month);
+RString LastWeekToString(int last_week_index);
+RString LastWeekToLocalizedString(int last_week_index);
+LuaDeclareType(Month);
 
-tm AddDays( tm start, int iDaysToMove );
-tm GetYesterday( tm start );
-int GetDayOfWeek( tm time );
-tm GetNextSunday( tm start );
+tm AddDays(tm start, int days_to_move);
+tm GetYesterday(tm start);
+int GetDayOfWeek(tm time);
+tm GetNextSunday(tm start);
 
-tm GetDayInYearAndYear( int iDayInYearIndex, int iYear );
+tm GetDayInYearAndYear(int day_in_year_index, int iYear);
 
-/** @brief A standard way of determining the date and the time. */
-struct DateTime 
-{
-	/**
-	 * @brief The number of seconds after the minute.
-	 *
-	 * Valid values are [0, 59]. */
-	int tm_sec;
-	/**
-	 * @brief The number of minutes after the hour.
-	 *
-	 * Valid values are [0, 59]. */
-	int tm_min;
-	/**
-	 * @brief The number of hours since midnight (or 0000 hours).
-	 *
-	 * Valid values are [0, 23]. */
-	int tm_hour;
-	/**
-	 * @brief The specified day of the current month.
-	 *
-	 * Valid values are [1, 31].
-	 *
-	 * XXX: Is it possible to set an illegal date through here,
-	 * such as day 30 of February? -Wolfman2000 */
-	int tm_mday;
-	/**
-	 * @brief The number of months since January.
-	 *
-	 * Valid values are [0, 11]. */
-	int tm_mon;
-	/** @brief The number of years since the year 1900. */
-	int tm_year;
+// A standard way of determining the date and the time.
+struct DateTime {
+  // The number of seconds after the minute.
+  // Valid values are [0, 59].
+  int tm_sec;
+  // The number of minutes after the hour.
+  // Valid values are [0, 59].
+  int tm_min;
+  // The number of hours since midnight (or 0000 hours).
+  // Valid values are [0, 23].
+  int tm_hour;
+  // The specified day of the current month.
+  // Valid values are [1, 31].
+  // NOTE(Wolfman2000): Is it possible to set an illegal date through here,
+  // such as day 30 of February?
+  int tm_mday;
+  // The number of months since January.
+  // Valid values are [0, 11].
+  int tm_mon;
+  // The number of years since the year 1900.
+  int tm_year;
 
-	/** @brief Set up a default date and time. */
-	DateTime();
-	/** @brief Initialize the date and time. */
-	void Init();
+  // Set up a default date and time.
+  DateTime();
+  // Initialize the date and time.
+  void Init();
 
-	/**
-	 * @brief Determine if this DateTime is less than some other time.
-	 * @param other the other DateTime to check.
-	 * @return true if this is less than the other time, or false otherwise. */
-	bool operator<( const DateTime& other ) const;
-	/**
-	 * @brief Determine if this DateTime is greater than some other time.
-	 * @param other the other DateTime to check.
-	 * @return true if this is greater than the other time, or false otherwise. */
-	bool operator>( const DateTime& other ) const;
-	/**
-	 * @brief Determine if this DateTime is equal to some other time.
-	 * @param other the other DateTime to check.
-	 * @return true if this is equal to the other time, or false otherwise. */
-	bool operator==( const DateTime& other ) const;
-	/**
-	 * @brief Determine if this DateTime is not equal to some other time.
-	 * @param other the other DateTime to check.
-	 * @return true if this is not equal to the other time, or false otherwise. */
-	bool operator!=( const DateTime& other ) const { return !operator==(other); }
-	/**
-	 * @brief Determine if this DateTime is less than or equal to some other time.
-	 * @param other the other DateTime to check.
-	 * @return true if this is less than or equal to the other time, or false otherwise. */
-	bool operator<=( const DateTime& other ) const { return !operator>(other); }
-	
-	/**
-	 * @brief Determine if this DateTime is greater than or equal to some other time.
-	 * @param other the other DateTime to check.
-	 * @return true if this is greater than or equal to the other time, or false otherwise. */
-	bool operator>=( const DateTime& other ) const { return !operator<(other); }
+  // Determine if this DateTime is less than some other time.
+  bool operator<(const DateTime& other) const;
+  // Determine if this DateTime is greater than some other time.
+  bool operator>(const DateTime& other) const;
+  // Determine if this DateTime is equal to some other time.
+  bool operator==(const DateTime& other) const;
+  // Determine if this DateTime is not equal to some other time.
+  bool operator!=(const DateTime& other) const { return !operator==(other); }
+  // Determine if this DateTime is less than or equal to some other time.
+  bool operator<=(const DateTime& other) const { return !operator>(other); }
 
-	/** 
-	 * @brief Retrieve the current date and time.
-	 * @return the current date and time. */
-	static DateTime GetNowDateTime();
-	/**
-	 * @brief Retrieve the current date.
-	 * @return the current date. */
-	static DateTime GetNowDate();
+  // Determine if this DateTime is greater than or equal to some other time.
+  bool operator>=(const DateTime& other) const { return !operator<(other); }
 
-	/** @brief Remove the time portion from the date. */
-	void StripTime();
+  // Retrieve the current date and time.
+  static DateTime GetNowDateTime();
+  // Retrieve the current date.
+  static DateTime GetNowDate();
 
-	/**
-	 * @brief Retrieve a string representation of the current date and time.
-	 *
-	 * This returns a common SQL/XML format: "YYYY-MM-DD HH:MM:SS".
-	 * @return the string representation of the date and time. */
-	RString GetString() const;
-	/**
-	 * @brief Attempt to turn a string into a DateTime.
-	 *
-	 * @param sDateTime the string to attempt to convert.
-	 * @return true if the conversion worked, or false otherwise. */
-	bool FromString( const RString sDateTime );
+  // Remove the time portion from the date.
+  void StripTime();
+
+  // Retrieve a string representation of the current date and time.
+  // This returns a common SQL/XML format: "YYYY-MM-DD HH:MM:SS".
+  RString GetString() const;
+  // Attempt to turn a string into a DateTime.
+  bool FromString(const RString date_time);
 };
 
-#endif
+#endif  // DATE_TIME_H
 
 /**
  * @file
  * @author Chris Danford (c) 2001-2004
  * @section LICENSE
  * All rights reserved.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -165,7 +122,7 @@ struct DateTime
  * copyright notice(s) and this permission notice appear in all copies of
  * the Software and that both the above copyright notice(s) and this
  * permission notice appear in supporting documentation.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
