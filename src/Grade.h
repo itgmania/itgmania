7e2c389a8087cc9d4316b06e5b4bfd6992a6eba8
teejusb <5017202+teejusb@users.noreply.github.com>
2023-05-01 21:15:39 -0700

    Update Grade to the Google Style Guide

--- src/Grade.h
+++ src/Grade.h
@@ -1,91 +1,80 @@
 #ifndef GRADE_H
 #define GRADE_H
 
-#include "RageUtil.h"
 #include "EnumHelper.h"
+#include "RageUtil.h"
 #include "ThemeMetric.h"
 
-/** @brief The list of grading tiers available.
- *
- * TODO: Look into a more flexible system without a fixed number of grades. -Wolfman2000
- */
-enum Grade
-{ 
-	Grade_Tier01,	/**< Usually an AAAA */
-	Grade_Tier02,	/**< Usually an AAA */
-	Grade_Tier03,	/**< Usually an AA */
-	Grade_Tier04,	/**< Usually an A */
-	Grade_Tier05,	/**< Usually a B */
-	Grade_Tier06,	/**< Usually a C */
-	Grade_Tier07,	/**< Usually a D */
-	Grade_Tier08,
-	Grade_Tier09,
-	Grade_Tier10,
-	Grade_Tier11,
-	Grade_Tier12,
-	Grade_Tier13,
-	Grade_Tier14,
-	Grade_Tier15,
-	Grade_Tier16,
-	Grade_Tier17,
-	Grade_Tier18,
-	Grade_Tier19,
-	Grade_Tier20,
-	Grade_Failed,	/**< Usually an E */
-	NUM_Grade, 
-	Grade_Invalid,
+// The list of grading tiers available.
+// TODO(Wolfman2000): Look into a more flexible system without a fixed number of
+// grades.
+enum Grade {
+  Grade_Tier01,  // Usually an AAAA
+  Grade_Tier02,  // Usually an AAA
+  Grade_Tier03,  // Usually an AA
+  Grade_Tier04,  // Usually an A
+  Grade_Tier05,  // Usually a B
+  Grade_Tier06,  // Usually a C
+  Grade_Tier07,  // Usually a D
+  Grade_Tier08,
+  Grade_Tier09,
+  Grade_Tier10,
+  Grade_Tier11,
+  Grade_Tier12,
+  Grade_Tier13,
+  Grade_Tier14,
+  Grade_Tier15,
+  Grade_Tier16,
+  Grade_Tier17,
+  Grade_Tier18,
+  Grade_Tier19,
+  Grade_Tier20,
+  Grade_Failed,  // Usually an E
+  NUM_Grade,
+  Grade_Invalid,
 };
-/** @brief Have an alternative for if there is no data for grading. */
+
+// Have an alternative for if there is no data for grading.
 #define Grade_NoData Grade_Invalid
 
-/**
- * @brief Convert the grade supplied to a string representation.
- *
- * This is in the header so the test sets don't require Grade.cpp (through PrefsManager),
- * since that pulls in ThemeManager.
- * @param g the grade to convert.
- * @return the string reprsentation.
- */
-static inline RString GradeToString( Grade g )
-{
-	ASSERT_M( (g >= 0 && g<NUM_Grade) || g == Grade_NoData, ssprintf("grade = %d",g) );
+// Convert the grade supplied to a string representation.
+//
+// This is in the header so the test sets don't require Grade.cpp (through
+// PrefsManager), since that pulls in ThemeManager.
+static inline RString GradeToString(Grade grade) {
+  ASSERT_M(
+      (grade >= 0 && grade < NUM_Grade) || grade == Grade_NoData,
+      ssprintf("grade = %d", grade));
 
-	switch( g )
-	{
-	case Grade_NoData:	return "NoData";
-	case Grade_Failed:	return "Failed";
-	default:		return ssprintf("Tier%02d",g+1);
-	}
+  switch (grade) {
+    case Grade_NoData:
+      return "NoData";
+    case Grade_Failed:
+      return "Failed";
+    default:
+      return ssprintf("Tier%02d", grade + 1);
+  }
 }
 
-/**
- * @brief Convert to the old version styled grade strings.
- *
- * This is mainly for backward compatibility purposes, but the announcer
- * also uses it. Think "AAA", "B", etc.
- * This is only referenced in ScreenEvaluation at the moment.
- * @param g the current Grade.
- * @return the old styled grade string. */
-RString GradeToOldString( Grade g );
-Grade GradeToOldGrade( Grade g );
-RString GradeToLocalizedString( Grade g );
-/**
- * @brief Convert the given RString into a proper Grade.
- * @param s the string to convert.
- * @return the expected Grade.
- */
-Grade StringToGrade( const RString &s );
-LuaDeclareType( Grade );
+// Convert to the old version styled grade strings.
+//
+// This is mainly for backward compatibility purposes, but the announcer
+// also uses it. Think "AAA", "B", etc.
+// This is only referenced in ScreenEvaluation at the moment.
+RString GradeToOldString(Grade grade);
+Grade GradeToOldGrade(Grade grade);
+RString GradeToLocalizedString(Grade grade);
+// Convert the given RString into a proper Grade.
+Grade StringToGrade(const RString& grade_str);
+LuaDeclareType(Grade);
 extern ThemeMetric<int> NUM_GRADE_TIERS_USED;
-#define NUM_POSSIBLE_GRADES	(NUM_GRADE_TIERS_USED+1)
-/**
- * @brief Step through the enumerator one at a time to get the next Grade.
- * @param g the current Grade.
- * @return the next Grade. */
-Grade GetNextPossibleGrade( Grade g );
-/** @brief Loop through each possible Grade. */
-#define FOREACH_PossibleGrade( g ) \
-for( Grade g = (Grade)(0); g != Grade_Invalid; g = GetNextPossibleGrade(g) )
+#define NUM_POSSIBLE_GRADES (NUM_GRADE_TIERS_USED + 1)
+// Step through the enumerator one at a time to get the next Grade.
+Grade GetNextPossibleGrade(Grade grade);
+// Loop through each possible Grade.
+#define FOREACH_PossibleGrade(grade)                     \
+  for (Grade grade = (Grade)(0); grade != Grade_Invalid; \
+       g = GetNextPossibleGrade(grade))
 
 #endif
 
@@ -94,7 +83,7 @@ for( Grade g = (Grade)(0); g != Grade_Invalid; g = GetNextPossibleGrade(g) )
  * @author Chris Danford (c) 2001-2004
  * @section LICENSE
  * All rights reserved.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -104,7 +93,7 @@ for( Grade g = (Grade)(0); g != Grade_Invalid; g = GetNextPossibleGrade(g) )
  * copyright notice(s) and this permission notice appear in all copies of
  * the Software and that both the above copyright notice(s) and this
  * permission notice appear in supporting documentation.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF
