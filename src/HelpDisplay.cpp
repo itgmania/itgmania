cae16a3fb5242f677fd17f2b530555f864785da2
teejusb <5017202+teejusb@users.noreply.github.com>
2023-05-01 22:18:20 -0700

    Update HelpDisplay to the Google Style Guide

--- src/HelpDisplay.cpp
+++ src/HelpDisplay.cpp
@@ -1,131 +1,131 @@
 #include "global.h"
+
 #include "HelpDisplay.h"
-#include "RageUtil.h"
-#include "RageLog.h"
-#include "ThemeManager.h"
-#include "ActorUtil.h"
 
 #include <vector>
 
+#include "ActorUtil.h"
+#include "RageLog.h"
+#include "RageUtil.h"
+#include "ThemeManager.h"
 
-REGISTER_ACTOR_CLASS( HelpDisplay );
+REGISTER_ACTOR_CLASS(HelpDisplay);
 
-HelpDisplay::HelpDisplay()
-{
-	m_iCurTipIndex = 0;
-	m_fSecsUntilSwitch = 0;
-	m_fSecsBetweenSwitches = 1;
+HelpDisplay::HelpDisplay() {
+  cur_tip_index_ = 0;
+  seconds_until_switch_ = 0;
+  seconds_between_switches_ = 1;
 }
 
-void HelpDisplay::Load( const RString &sType )
-{
-	RunCommands( THEME->GetMetricA(sType,"TipOnCommand") );
-	m_fSecsUntilSwitch = THEME->GetMetricF(sType,"TipShowTime");
-	m_fSecsBetweenSwitches = THEME->GetMetricF(sType,"TipSwitchTime");
+void HelpDisplay::Load(const RString& type) {
+  RunCommands(THEME->GetMetricA(type, "TipOnCommand"));
+  seconds_until_switch_ = THEME->GetMetricF(type, "TipShowTime");
+  seconds_between_switches_ = THEME->GetMetricF(type, "TipSwitchTime");
 }
 
-void HelpDisplay::SetTips( const std::vector<RString> &arrayTips, const std::vector<RString> &arrayTipsAlt )
-{
-	ASSERT( arrayTips.size() == arrayTipsAlt.size() );
+void HelpDisplay::SetTips(
+    const std::vector<RString>& array_tips,
+    const std::vector<RString>& array_tips_alt) {
+  ASSERT(array_tips.size() == array_tips_alt.size());
 
-	if( arrayTips == m_arrayTips && arrayTipsAlt == m_arrayTipsAlt )
-		return;
+  if (array_tips == array_tips_ && array_tips_alt == array_tips_alt_) {
+    return;
+  }
 
-	SetText( "" );
+  SetText("");
 
-	m_arrayTips = arrayTips;
-	m_arrayTipsAlt = arrayTipsAlt;
+  array_tips_ = array_tips;
+  array_tips_alt_ = array_tips_alt;
 
-	m_iCurTipIndex = 0;
-	m_fSecsUntilSwitch = 0;
-	Update( 0 );
+  cur_tip_index_ = 0;
+  seconds_until_switch_ = 0;
+  Update(0);
 }
 
+void HelpDisplay::Update(float delta) {
+  float hibernate = m_fHibernateSecondsLeft;
 
-void HelpDisplay::Update( float fDeltaTime )
-{
-	float fHibernate = m_fHibernateSecondsLeft;
+  BitmapText::Update(delta);
 
-	BitmapText::Update( fDeltaTime );
+  if (array_tips_.empty()) {
+    return;
+  }
 
-	if( m_arrayTips.empty() )
-		return;
+  seconds_until_switch_ -= std::max(delta - hibernate, 0.0f);
+  if (seconds_until_switch_ > 0) {
+    return;
+  }
 
-	m_fSecsUntilSwitch -= std::max( fDeltaTime - fHibernate, 0.0f );
-	if( m_fSecsUntilSwitch > 0 )
-		return;
-
-	// time to switch states
-	m_fSecsUntilSwitch = m_fSecsBetweenSwitches;
-	SetText( m_arrayTips[m_iCurTipIndex], m_arrayTipsAlt[m_iCurTipIndex] );
-	m_iCurTipIndex++;
-	m_iCurTipIndex = m_iCurTipIndex % m_arrayTips.size();
+  // Time to switch states.
+  seconds_until_switch_ = seconds_between_switches_;
+  SetText(array_tips_[cur_tip_index_], array_tips_alt_[cur_tip_index_]);
+  cur_tip_index_++;
+  cur_tip_index_ = cur_tip_index_ % array_tips_.size();
 }
 
-
-#include "LuaBinding.h"
 #include "FontCharAliases.h"
+#include "LuaBinding.h"
 
-/** @brief Allow Lua to have access to the HelpDisplay. */
-class LunaHelpDisplay: public Luna<HelpDisplay>
-{
-public:
-	static int settips( T* p, lua_State *L )
-	{
-		luaL_checktype( L, 1, LUA_TTABLE );
-		lua_pushvalue( L, 1 );
-		std::vector<RString> arrayTips;
-		LuaHelpers::ReadArrayFromTable( arrayTips, L );
-		lua_pop( L, 1 );
-		for( unsigned i = 0; i < arrayTips.size(); ++i )
-			FontCharAliases::ReplaceMarkers( arrayTips[i] );
-		if( lua_gettop(L) > 1 && !lua_isnil( L, 2 ) )
-		{
-			std::vector<RString> arrayTipsAlt;
-			luaL_checktype( L, 2, LUA_TTABLE );
-			lua_pushvalue( L, 2 );
-			LuaHelpers::ReadArrayFromTable( arrayTipsAlt, L );
-			lua_pop( L, 1 );
-			for( unsigned i = 0; i < arrayTipsAlt.size(); ++i )
-				FontCharAliases::ReplaceMarkers( arrayTipsAlt[i] );
+// Allow Lua to have access to the HelpDisplay.
+class LunaHelpDisplay : public Luna<HelpDisplay> {
+ public:
+  static int settips(T* p, lua_State* L) {
+    luaL_checktype(L, 1, LUA_TTABLE);
+    lua_pushvalue(L, 1);
+    std::vector<RString> array_tips;
+    LuaHelpers::ReadArrayFromTable(array_tips, L);
+    lua_pop(L, 1);
+    for (unsigned i = 0; i < array_tips.size(); ++i) {
+      FontCharAliases::ReplaceMarkers(array_tips[i]);
+    }
+    if (lua_gettop(L) > 1 && !lua_isnil(L, 2)) {
+      std::vector<RString> array_tips_alt;
+      luaL_checktype(L, 2, LUA_TTABLE);
+      lua_pushvalue(L, 2);
+      LuaHelpers::ReadArrayFromTable(array_tips_alt, L);
+      lua_pop(L, 1);
+      for (unsigned i = 0; i < array_tips_alt.size(); ++i) {
+        FontCharAliases::ReplaceMarkers(array_tips_alt[i]);
+      }
 
-			p->SetTips( arrayTips, arrayTipsAlt );
-		}
-		else
-			p->SetTips( arrayTips );
+      p->SetTips(array_tips, array_tips_alt);
+    } else {
+      p->SetTips(array_tips);
+    }
 
-		COMMON_RETURN_SELF;
-	}
-	static int SetTipsColonSeparated( T* p, lua_State *L )
-	{
-		std::vector<RString> vs;
-		split( SArg(1), "::", vs );
-		p->SetTips( vs );
-		COMMON_RETURN_SELF;
-	}
+    COMMON_RETURN_SELF;
+  }
+  static int SetTipsColonSeparated(T* p, lua_State* L) {
+    std::vector<RString> tips;
+    split(SArg(1), "::", tips);
+    p->SetTips(tips);
+    COMMON_RETURN_SELF;
+  }
 
-	static int gettips( T* p, lua_State *L )
-	{
-		std::vector<RString> arrayTips, arrayTipsAlt;
-		p->GetTips( arrayTips, arrayTipsAlt );
+  static int gettips(T* p, lua_State* L) {
+    std::vector<RString> array_tips;
+		std::vector<RString> array_tips_alt;
+    p->GetTips(array_tips, array_tips_alt);
 
-		LuaHelpers::CreateTableFromArray( arrayTips, L );
-		LuaHelpers::CreateTableFromArray( arrayTipsAlt, L );
+    LuaHelpers::CreateTableFromArray(array_tips, L);
+    LuaHelpers::CreateTableFromArray(array_tips_alt, L);
 
-		return 2;
-	}
-	static int SetSecsBetweenSwitches( T* p, lua_State *L ) { p->SetSecsBetweenSwitches( FArg(1) ); COMMON_RETURN_SELF; }
+    return 2;
+  }
+  static int SetSecsBetweenSwitches(T* p, lua_State* L) {
+    p->SetSecsBetweenSwitches(FArg(1));
+    COMMON_RETURN_SELF;
+  }
 
-	LunaHelpDisplay()
-	{
-		ADD_METHOD( settips );
-		ADD_METHOD( SetTipsColonSeparated );
-		ADD_METHOD( gettips );
-		ADD_METHOD( SetSecsBetweenSwitches );
-	}
+  LunaHelpDisplay() {
+    ADD_METHOD(settips);
+    ADD_METHOD(SetTipsColonSeparated);
+    ADD_METHOD(gettips);
+    ADD_METHOD(SetSecsBetweenSwitches);
+  }
 };
 
-LUA_REGISTER_DERIVED_CLASS( HelpDisplay, BitmapText )
+LUA_REGISTER_DERIVED_CLASS(HelpDisplay, BitmapText)
 
 /*
  * (c) 2001-2003 Chris Danford, Glenn Maynard
