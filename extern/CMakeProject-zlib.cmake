0c5160dd1cf19a30e8c4459f722dc632e2b769d3
Ben Kirzhner <ben.kirzhner@gmail.com>
2023-05-22 11:11:51 -0400

    Use c standard w/o warnings on macOS >=13.3
      See itgmania/itgmania#107 for error details.

--- extern/CMakeProject-zlib.cmake
+++ extern/CMakeProject-zlib.cmake
@@ -32,6 +32,13 @@ add_library("zlib" STATIC ${ZLIB_SRC} ${ZLIB_HPP})
 
 set_property(TARGET "zlib" PROPERTY FOLDER "External Libraries")
 
+if(APPLE)
+  # Fixes build failures due to warnings on macOS >= 13.3
+  # See itgmania/itgmania#107 for details
+  set_property(TARGET "zlib" PROPERTY C_STANDARD 90)
+  set_property(TARGET "zlib" PROPERTY C_STANDARD_REQUIRED ON)
+endif(APPLE)
+
 disable_project_warnings("zlib")
 
 if(MSVC)
