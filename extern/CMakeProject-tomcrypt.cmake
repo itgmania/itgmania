b773f3d7c5c1b12f1cf73ebfefe2778a33a57a01
Ben Kirzhner <ben.kirzhner@gmail.com>
2023-05-22 14:58:52 -0400

    Add requested TODOs.

--- extern/CMakeProject-tomcrypt.cmake
+++ extern/CMakeProject-tomcrypt.cmake
@@ -474,6 +474,7 @@ if(APPLE)
 
   # Fixes build failures due to warnings on macOS >= 13.3
   # See itgmania/itgmania#107 for details
+  # TODO(teejusb/natano): Remove these two lines once these issues have been fixed upstream.
   set_property(TARGET "tomcrypt" PROPERTY C_STANDARD 90)
   set_property(TARGET "tomcrypt" PROPERTY C_STANDARD_REQUIRED ON)
 endif()
--- extern/CMakeProject-zlib.cmake
+++ extern/CMakeProject-zlib.cmake
@@ -35,6 +35,7 @@ set_property(TARGET "zlib" PROPERTY FOLDER "External Libraries")
 if(APPLE)
   # Fixes build failures due to warnings on macOS >= 13.3
   # See itgmania/itgmania#107 for details
+  # TODO(teejusb/natano): Remove these two lines once these issues have been fixed upstream.
   set_property(TARGET "zlib" PROPERTY C_STANDARD 90)
   set_property(TARGET "zlib" PROPERTY C_STANDARD_REQUIRED ON)
 endif(APPLE)
