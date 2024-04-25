0ebaffa6f174e79039267cbd600eb78f93222cdc
teejusb <5017202+teejusb@users.noreply.github.com>
2024-04-25 13:00:05 -0700

    Use the svg for the icon on linux + copy SL Modules as part of the setup script

--- Installer/setup.sh
+++ Installer/setup.sh
@@ -7,7 +7,12 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 copy_sl_config() {
-	if [ -e "/opt/itgmania/Themes/Simply Love.old/$1" ]; then
+	if [ -d "/opt/itgmania/Themes/Simply Love.old/$1" ]; then
+		# Don't overwrite any existing files.
+		# This lets us retain the updated readmes and other files that come with the new release.
+		cp -r -n "/opt/itgmania/Themes/Simply Love.old/$1" "/opt/itgmania/Themes/Simply Love/$1"
+		return
+	elif [ -f "/opt/itgmania/Themes/Simply Love.old/$1" ]; then
 		cp "/opt/itgmania/Themes/Simply Love.old/$1" "/opt/itgmania/Themes/Simply Love/$1"
 	fi
 }
@@ -26,6 +31,7 @@ ln -sf /opt/itgmania/itgmania.desktop /usr/share/applications
 if [ -d /opt/itgmania/Themes/Simply\ Love.old ]; then
 	copy_sl_config Other/SongManager\ PreferredCourses.txt
 	copy_sl_config Other/SongManager\ PreferredSongs.txt
+	copy_sl_config Modules
 
 	rm -rf /opt/itgmania/Themes/Simply\ Love.old
 fi
