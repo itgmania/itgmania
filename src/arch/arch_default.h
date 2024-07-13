2eb58857cc724c545b37a5a5dc5a334f388423b8
Brandon W <bwaggone@umich.edu>
2024-07-13 10:08:42 -0400

    Remove dshow movie rendering code.
    It hasn't been built into the binary in years, not since before forking
    itgmania.
    https://github.com/itgmania/itgmania/blob/5d4f9dcb07493ed4c51d6be23e9b41978162305a/src/CMakeData-arch.cmake#L81-L91
    https://github.com/stepmania/stepmania/blob/d55acb1ba26f1c5b5e3048d6d6c0bd116625216f/src/Makefile.am#L388
    
    This helps to consolidate the movie rendering code into one main
    rendering path.

--- src/arch/arch_default.h
+++ src/arch/arch_default.h
@@ -8,7 +8,7 @@
 #include "LowLevelWindow/LowLevelWindow_Win32.h"
 #include "MemoryCard/MemoryCardDriverThreaded_Windows.h"
 #define DEFAULT_INPUT_DRIVER_LIST "DirectInput,Pump,Para"
-#define DEFAULT_MOVIE_DRIVER_LIST "FFMpeg,DShow,Null"
+#define DEFAULT_MOVIE_DRIVER_LIST "FFMpeg,Null"
 #define DEFAULT_SOUND_DRIVER_LIST "WaveOut,DirectSound-sw,WDMKS,Null"
 
 
