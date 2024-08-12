a3cefd1ef1cdf2805f055e93ffe5c4f9c7cbb3ea
Rafał Florczak <florczakraf@users.noreply.github.com>
2024-08-12 18:20:08 +0200

    Revert "Migrate to universal build for macOS"
    This reverts commit b938ac449f3a2696063fdc83c6da277e4088a120.

--- Utils/build-release-macos.sh
+++ Utils/build-release-macos.sh
@@ -3,10 +3,14 @@ set -eux
 
 cd "$(dirname $0)/.."
 
-cmake -B Build/release-universal -DCMAKE_BUILD_TYPE=Release -DWITH_FULL_RELEASE=On -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DWITH_CLUB_FANTASTIC=On
-cmake --build Build/release-universal -j $(sysctl -n hw.logicalcpu)
-cmake --build Build/release-universal --target package
+cmake -B Build/release-arm64 -DCMAKE_BUILD_TYPE=Release -DWITH_FULL_RELEASE=On -DCMAKE_OSX_ARCHITECTURES=arm64 -DWITH_CLUB_FANTASTIC=On
+cmake --build Build/release-arm64 -j $(sysctl -n hw.logicalcpu)
+cmake --build Build/release-arm64 --target package
+cmake -B Build/release-arm64 -DCMAKE_BUILD_TYPE=Release -DWITH_FULL_RELEASE=On -DCMAKE_OSX_ARCHITECTURES=arm64 -DWITH_CLUB_FANTASTIC=Off
+cmake --build Build/release-arm64 --target package
 
-cmake -B Build/release-universal -DCMAKE_BUILD_TYPE=Release -DWITH_FULL_RELEASE=On -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DWITH_CLUB_FANTASTIC=Off
-cmake --build Build/release-universal -j $(sysctl -n hw.logicalcpu)
-cmake --build Build/release-universal --target package
+cmake -B Build/release-x86_64 -DCMAKE_BUILD_TYPE=Release -DWITH_FULL_RELEASE=On -DCMAKE_OSX_ARCHITECTURES=x86_64 -DWITH_CLUB_FANTASTIC=On
+cmake --build Build/release-x86_64 -j $(sysctl -n hw.logicalcpu)
+cmake --build Build/release-x86_64 --target package
+cmake -B Build/release-x86_64 -DCMAKE_BUILD_TYPE=Release -DWITH_FULL_RELEASE=On -DCMAKE_OSX_ARCHITECTURES=x86_64 -DWITH_CLUB_FANTASTIC=Off
+cmake --build Build/release-x86_64 --target package
