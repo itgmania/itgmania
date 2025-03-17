7b8ae9f6bd81cd820caf1b3a9d888a813eec9c16
DANDO\Aless <alessandro.dore@live.it>
2025-03-17 21:47:06 +0100

    Added cmake for both linux and win build

---
+++ extern/CMakeProject-hidapi.cmake
@@ -0,0 +1,34 @@
+set(HIDAPI_HPP "hidapi/hidapi/hidapi.h")
+
+if(APPLE)
+    set(HIDAPI_SRC "hidapi/mac/hid.c")
+	list(APPEND HIDAPI_HPP 
+			"hidapi/windows/hidapi_darwin.h")
+endif()
+
+if(WIN32)
+    set(HIDAPI_SRC "hidapi/windows/hid.c" "hidapi/windows/hidapi_descriptor_reconstruct.c")
+	list(APPEND HIDAPI_HPP 
+			"hidapi/windows/hidapi_cfgmgr32.h"
+			"hidapi/windows/hidapi_descriptor_reconstruct.h"
+			"hidapi/windows/hidapi_hidclass.h"
+			"hidapi/windows/hidapi_hidpi.h"
+			"hidapi/windows/hidapi_hidsdi.h"
+			"hidapi/windows/hidapi_winapi.h")
+endif()
+
+if(UNIX)
+    set(HIDAPI_SRC "hidapi/linux/hid.c")
+endif()
+
+source_group("" FILES ${HIDAPI_SRC} ${HIDAPI_HPP})
+
+add_library("hidapi" STATIC ${HIDAPI_SRC} ${HIDAPI_HPP})
+
+set_property(TARGET "hidapi" PROPERTY FOLDER "External Libraries")
+
+if(MSVC)
+  target_compile_definitions("hidapi" PRIVATE _CRT_SECURE_NO_WARNINGS)
+endif()
+
+target_include_directories("hidapi" PUBLIC "hidapi/hidapi")
