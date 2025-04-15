228b10edd29ca7291ed0c771c99a7fde4e473168
Caleb Lee <caleb.a.lee@icloud.com>
2025-04-15 12:50:45 -0400

    Removes SMXInfo struct, moves SMX_Start() back to private methods

--- src/arch/InputHandler/InputHandler_Win32_SMX.h
+++ src/arch/InputHandler/InputHandler_Win32_SMX.h
@@ -13,11 +13,6 @@ enum SMXUpdateCallbackReason {
 	SMXUpdateCallback_FactoryResetCommandComplete
 };
 typedef void SMXUpdateCallback(int pad, SMXUpdateCallbackReason reason, void* pUser);
-struct SMXInfo {
-    bool m_bConnected = false;
-	char m_Serial[33];
-	uint16_t m_iFirmwareVersion;
-};
 typedef void SMXLogCallback(const char *log);
 
 class InputHandler_Win32_SMX: public InputHandler
@@ -29,11 +24,10 @@ public:
 	RString GetDeviceSpecificInputString( const DeviceInput &di );
 	void ProcessInputEvent( int pad );
 
-	void SMX_Start();
-
 private:
 	uint16_t m_padInputStates[SMX_PAD_COUNT];
 
+	void SMX_Start();
     uint16_t SMX_GetInputState( int pad );
 	void SMX_SetLogCallback();
 	void SMX_Stop();
