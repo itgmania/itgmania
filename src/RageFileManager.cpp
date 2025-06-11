fb2b53bcbbf0a0b8ee621387113a136980912838
Sergio Perez Fernandez <sergioperez794@gmail.com>
2025-06-12 00:28:14 +0200

    do not try to create a directory when it exists

--- src/RageFileManager.cpp
+++ src/RageFileManager.cpp
@@ -663,6 +663,7 @@ bool RageFileManager::DeleteRecursive( const RString &sPath )
 
 void RageFileManager::CreateDir( const RString &sDir )
 {
+	if (DoesFileExist(sDir)) return;
 	RString sTempFile = sDir + "newdir.temp.newdir";
 	RageFile f;
 	f.Open( sTempFile, RageFile::WRITE );
