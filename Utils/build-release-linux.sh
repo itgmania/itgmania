4148240807d6039714eb4fcf295970e72c730194
Sergio Perez Fernandez <sergioperez794@gmail.com>
2025-07-22 01:21:55 +0200

    build-release-linux: fixed missing cmake variable for ARM

--- Utils/build-release-linux.sh
+++ Utils/build-release-linux.sh
@@ -9,12 +9,13 @@ docker build --platform=${ARCH} -f Dockerfile-linux . -t itgmania-linux-build:${
 
 docker run -i -v $(pwd)/..:/data:Z --platform=linux/${ARCH} itgmania-linux-build:${ARCH} sh -eux <<'EOF'
 WITH_MINIMAID=On ; [ "$(arch)" != "x86_64" ] && WITH_MINIMAID=Off
-export WITH_MINIMAID
 git config --global --add safe.directory /data
-cmake -S /data -B /tmp/Build -DCMAKE_BUILD_TYPE=Release -DWITH_FULL_RELEASE=On -DWITH_CLUB_FANTASTIC=On
+cmake -S /data -B /tmp/Build -DCMAKE_BUILD_TYPE=Release -DWITH_FULL_RELEASE=On \
+	-DWITH_CLUB_FANTASTIC=On -DWITH_MINIMAID=${WITH_MINIMAID}
 cmake --build /tmp/Build -j $(nproc)
 cmake --build /tmp/Build --target package
-cmake -S /data -B /tmp/Build -DCMAKE_BUILD_TYPE=Release -DWITH_FULL_RELEASE=On -DWITH_CLUB_FANTASTIC=Off
+cmake -S /data -B /tmp/Build -DCMAKE_BUILD_TYPE=Release -DWITH_FULL_RELEASE=On \
+	-DWITH_CLUB_FANTASTIC=Off -DWITH_MINIMAID=${WITH_MINIMAID}
 cmake --build /tmp/Build --target package
 mkdir -p /data/Build/release
 cp /tmp/Build/ITGmania-* /data/Build/release/
