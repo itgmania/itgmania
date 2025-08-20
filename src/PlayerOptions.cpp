a5c43ad24220c43bf939890c764941e60f326638
ecourt93 <e.court93@gmail.com>
2025-08-20 15:18:07 -0400

    Only disqualify if BG or FG Changes exist

--- src/PlayerOptions.cpp
+++ src/PlayerOptions.cpp
@@ -1680,6 +1680,10 @@ bool PlayerOptions::IsEasierForSongAndSteps( Song* pSong, Steps* pSteps, PlayerN
 	if ((m_fNoAttack && pSteps->HasAttacks()) || m_fRandAttack)
 		return true;
 
+	if( m_fCover && (!pSong->GetBackgroundChanges().empty() || !pSong->GetForegroundChanges().empty())) {
+		return true;
+	}
+	
 	// M-mods make songs with indefinite BPMs easier because
 	// they ensure that the song has a scrollable speed.
 	if( m_fMaxScrollBPM != 0 )
