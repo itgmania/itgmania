ec18cb479c66f2927bff1eac2413377082d2b718
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2026-01-26 12:58:02 -0500

    Remove unneeded test changes

--- src/ScreenSelectMusic.cpp
+++ src/ScreenSelectMusic.cpp
@@ -1164,7 +1164,6 @@ void ScreenSelectMusic::HandleScreenMessage( const ScreenMessage SM )
 		}
 		else if( DO_ROULETTE_ON_MENU_TIMER  &&  m_MusicWheel.GetSelectedSong() == nullptr  &&  m_MusicWheel.GetSelectedCourse() == nullptr )
 		{
-			m_MusicWheel.SetOpenSection( "" );
 			m_MusicWheel.StartRoulette();
 			m_MenuTimer->SetSeconds( ROULETTE_TIMER_SECONDS );
 			m_MenuTimer->Start();
