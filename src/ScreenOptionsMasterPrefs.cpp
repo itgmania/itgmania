24d3acb8592921961403d4ef9225e2271c4c526d
andrbmgi <andrbmgi@users.noreply.github.com>
2026-02-18 22:25:45 +0100

    Fix formatting in SampleMusicVolume function

--- src/ScreenOptionsMasterPrefs.cpp
+++ src/ScreenOptionsMasterPrefs.cpp
@@ -697,8 +697,8 @@ static void SoundVolumeAttract( int &sel, bool ToSel, const ConfOption *pConfOpt
 
 static void SampleMusicVolume( int &sel, bool ToSel, const ConfOption *pConfOption )
 {
-        const float mapping[] = { 0.0f,0.1f,0.2f,0.3f,0.4f,0.5f,0.6f,0.7f,0.8f,0.9f,1.0f };
-        MoveMap( sel, pConfOption, ToSel, mapping, ARRAYLEN(mapping) );
+	const float mapping[] = { 0.0f,0.1f,0.2f,0.3f,0.4f,0.5f,0.6f,0.7f,0.8f,0.9f,1.0f };
+	MoveMap( sel, pConfOption, ToSel, mapping, ARRAYLEN(mapping) );
 }
 
 static void PreferredSampleRate( int &sel, bool ToSel, const ConfOption *pConfOption )
