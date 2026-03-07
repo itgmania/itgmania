81bc43f4c30f0cda14725b79dacc6be54c665dfe
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2026-03-07 15:02:38 -0500

    Add some "stock" couples noteskins!

---
+++ NoteSkins/dance/couples/Down Hold Head Inactive.lua
@@ -0,0 +1,7 @@
+local t = Def.Model {
+	Meshes=NOTESKIN:GetPath('_down','tap note hold model');
+	Materials=NOTESKIN:GetPath('_down','tap note hold model');
+	Bones=NOTESKIN:GetPath('_down','tap note hold model');
+};
+
+return t;
---
+++ NoteSkins/dance/couples/Down Roll Head Inactive.lua
@@ -0,0 +1,7 @@
+local t = Def.Model {
+	Meshes=NOTESKIN:GetPath('_down','tap note roll model');
+	Materials=NOTESKIN:GetPath('_down','tap note roll model');
+	Bones=NOTESKIN:GetPath('_down','tap note roll model');
+};
+
+return t;
