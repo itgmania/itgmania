4b8ace1b1d3930cb88b092cc561656d8f36dfad0
Arthur Eubanks <ayueubanks@gmail.com>
2026-05-17 08:55:53 -0700

    Fix macOS crash dir
    We delete the old Logs dir, but never updated the macOS crash handler to
    point to the new Logs dir.
    
    Verified that this generates crashinfo.txt in the correct dir instead of
    not being able to open it.

--- src/archutils/Darwin/Crash.mm
+++ src/archutils/Darwin/Crash.mm
@@ -18,7 +18,7 @@
 
 std::string CrashHandler::GetLogsDirectory() {
   NSFileManager* fileManager = [NSFileManager defaultManager];
-  NSURL* url = [fileManager URLForDirectory:NSLibraryDirectory
+  NSURL* url = [fileManager URLForDirectory:NSApplicationSupportDirectory
                                    inDomain:NSUserDomainMask
                           appropriateForURL:nil
                                      create:NO
@@ -27,8 +27,7 @@ std::string CrashHandler::GetLogsDirectory() {
     return "/tmp";
   }
 
-  std::string path = std::string([url fileSystemRepresentation]) + "/Logs/" PRODUCT_ID;
-  return std::string(path.c_str());
+  return std::string([url fileSystemRepresentation]) + "/" + PRODUCT_ID + "/Logs";
 }
 
 // XXX Can we use LocalizedString here instead?
